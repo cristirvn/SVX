dd668e45b04f987c80da12953d272f673fe412f3
cristirvn <168576131+cristirvn@users.noreply.github.com>
2024-12-17 16:32:26 +0200

    added env variable for sport input

--- SVX-v1.0.0/Crawler.py
+++ SVX-v1.0.0/Crawler.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from typing import List
 import configparser
-
+import os
 from sqlalchemy import create_engine, Column, String, Integer, CHAR, TIMESTAMP, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -50,68 +50,68 @@ def retrieve_country_links(driver, list_of_links, sport):
         division = name[5]
         list_of_links.append(ChampionshipLinks(sport, country, division))
 
+if __name__ == "main":
+    sport = os.getenv("SPORT", "basketball")
+    sport_url = f"https://www.flashscore.com/{sport}/"
+    list_of_links : List[ChampionshipLinks] = []
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-sport = input("Enter the sport: ")
-sport_url = f"https://www.flashscore.com/{sport}/"
-list_of_links : List[ChampionshipLinks] = []
-# chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    path = r"C:\Users\User\Desktop\workspace2\chromedriver-win64\chromedriver.exe"
+    service = Service(path)
+    driver = webdriver.Chrome(service=service)
 
-path = r"C:\Users\User\Desktop\workspace2\chromedriver-win64\chromedriver.exe"
-service = Service(path)
-driver = webdriver.Chrome(service=service)
+    driver.get(sport_url)
 
-driver.get(sport_url)
+    retrieve_country_links(driver, list_of_links, sport)
+    driver.quit()
 
-retrieve_country_links(driver, list_of_links, sport)
-driver.quit()
+    #created a table in database using sqlalchemy to store the championship links
+    Base = declarative_base()
 
-#created a table in database using sqlalchemy to store the championship links
-Base = declarative_base()
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+    db_user = config["database"]["user"]
+    db_password = config["database"]["password"]
+    db_host = config["database"]["host"]
+    db_port = config["database"]["port"]
+    db_name = config["database"]["dbname"]
 
-db_user = config["database"]["user"]
-db_password = config["database"]["password"]
-db_host = config["database"]["host"]
-db_port = config["database"]["port"]
-db_name = config["database"]["dbname"]
+    # Create the database URL dynamically
+    database_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-# Create the database URL dynamically
-database_url = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    class Link(Base):
+        __tablename__ = "championship_links"
 
-class Link(Base):
-    __tablename__ = "championship_links"
+        id = Column("id", Integer, primary_key=True, autoincrement=True)
+        url = Column("url", String)
+        sport = Column("sport", String)  
+        country = Column("country", String)
+        league = Column("league", String)
+        Created_at = Column(TIMESTAMP, server_default=func.now())  # Automatically set timestamp
 
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    url = Column("url", String)
-    sport = Column("sport", String)  
-    country = Column("country", String)
-    league = Column("league", String)
-    Created_at = Column(TIMESTAMP, server_default=func.now())  # Automatically set timestamp
-
-    def __init__(self, url, sport, country, league):
-        self.url = url
-        self.sport = sport
-        self.country = country
-        self.league = league
+        def __init__(self, url, sport, country, league):
+            self.url = url
+            self.sport = sport
+            self.country = country
+            self.league = league
 
 
-engine = create_engine(database_url, echo = True)
-Base.metadata.create_all(bind = engine)
+    engine = create_engine(database_url, echo = True)
+    Base.metadata.create_all(bind = engine)
 
-Session = sessionmaker(bind = engine)
-session = Session()
+    Session = sessionmaker(bind = engine)
+    session = Session()
 
-#adding the championships links in the data base
-# session.query(Link).filter(Link.id >= 407, Link.id <= 808).delete(synchronize_session=False)
+    #adding the championships links in the data base
+    # session.query(Link).filter(Link.id >= 407, Link.id <= 808).delete(synchronize_session=False)
 
-for ch in list_of_links:
-    official_link = ch.get_link_prefix()
-    element = Link(official_link, sport, ch.country, ch.division)
-    session.add(element)
+    for ch in list_of_links:
+        official_link = ch.get_link_prefix()
+        element = Link(official_link, sport, ch.country, ch.division)
+        session.add(element)
 
-session.commit()
-session.close()
+    session.commit()
+    session.close()
